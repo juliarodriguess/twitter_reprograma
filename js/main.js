ab8e5f91062b7a1590249a07667aaff6bb03d117
Julia Rodrigues <srodriguesjulia@gmail.com>
2018-10-17 09:16:53 -0300

    Refatorar código

--- js/main.js
+++ js/main.js
@@ -1,12 +1,6 @@
-let button = $("#btn-submit");
-button.click(function () {
-    let text = $("#text-tweet").val();
-    let fim = $("#fim");
-    let tweet =
-    `<div>
-    <p class="h3 text-center mt-5">${text}</p>
-    </div>`
-    fim.prepend(tweet);
-    console.log(text, tweet);
+$("#btn-submit").click(function () {
+    $("#fim").prepend(`<div>
+    <p class="h3 text-center mt-5">${$("#text-tweet").val()}</p>
+    </div>`);
 });
 
