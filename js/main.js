6d236768e381e79751b31ef1339e2af13afd2c26
Julia Rodrigues <srodriguesjulia@gmail.com>
2018-10-17 22:35:29 -0300

    Criar layout para tweets

--- js/main.js
+++ js/main.js
@@ -1,6 +1,18 @@
 $("#btn-submit").click(function () {
-    $("#fim").prepend(`<div>
-    <p class="h3 text-center mt-5">${$("#text-tweet").val()}</p>
-    </div>`);
+    let data = new Date;
+    $("#tweets").prepend(
+        `<section class="card bg-light mt-5 w-50">
+            <div class="card-header">@vc ${data.getDate()}/${data.getMonth()+1}/${data.getFullYear()} ${data.getHours()}:${data.getMinutes()}</div>
+            <div class="card-body">
+            <p class="card-text">${$("#text-tweet").val()}</p>
+            </div>
+        </section>`
+    );
+    $("#text-tweet").val("");
 });
 
+$("#text-tweet").keyup(function(){
+    let count = 280 - $("#text-tweet").val().length;
+    let text = $("#contador").text(`Restam ${count} pios`);
+    return text;
+})
